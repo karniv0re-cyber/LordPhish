3a8d47673620fb38ce4838284405aefadff823e7
Gr3n0xX <74163590+Ch33chOficial@users.noreply.github.com>
2021-01-24 20:46:26 -0300

    update lord.sh

--- lord.sh
+++ lord.sh
@@ -74,7 +74,6 @@ command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not instal
 command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
 command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
-command -v npm > /dev/null 2>&1 || { echo >&2 "I require npm but it's not installed. Install it. Aborting."; exit 1; }
 
 }
 
